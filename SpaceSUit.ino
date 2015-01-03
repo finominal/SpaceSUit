fcccae1c9e93becaaa6f77ae840b218dc56a645e
Finominal <finianliam@hotmail.com>
2015-01-03 22:34:02 +1100

    ROTATOR TOGGLE RELAY
    .

--- SpaceSUit.ino
+++ SpaceSUit.ino
@@ -1,4 +1,5 @@
 #include <FastLED.h>
+
 #define NUM_LEDS 6
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
@@ -9,42 +10,95 @@
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-
-
-
-int UP, DOWN, LEFT, RIGHT, A, B;
-
+boolean UP, DOWN, LEFT, RIGHT, A, B;
+ 
+boolean rotatorOn = false; 
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   FastLED.addLeds<NEOPIXEL,DATA_PIN>(leds, NUM_LEDS);
+  
+  pinMode(4,OUTPUT);
 
 }
 
 void loop()
 {
   ReadButtons();
+  ShowButtons();
+  ToggleRotator();
+}
+
+void ToggleRotator()
+{
+  if (B == true)
+  {
+   rotatorOn = !rotatorOn;
+  }
   
-  if(LEFT <500) {leds[0] = CRGB::Red;} else {leds[0] = CRGB::Black;}
-  if(RIGHT <500) {leds[1] = CRGB::Red;}else {leds[1] = CRGB::Black;}
-  if(UP <500) {leds[2] = CRGB::Red;}else {leds[2] = CRGB::Black;}
-  if(DOWN <500) {leds[3] = CRGB::Red;}else {leds[3] = CRGB::Black;}
-  if(A <500) {leds[4] = CRGB::Red;}else {leds[4] = CRGB::Black;}
-  if(B <500) {leds[5] = CRGB::Red;}else {leds[5] = CRGB::Black;}
+  if(rotatorOn == true)
+  {
+    digitalWrite(4,LOW);
+  }
+  else
+  {
+    digitalWrite(4,HIGH);
+  }
+  
+  while(B == true)
+ {
+   ReadButtons();
+ } 
+
+ Serial.println("Exit B Button Press");
+ delay(5);
+}
+  
+void ShowButtons()
+{
+  
+  if(LEFT == true) {leds[0] = CRGB::SeaGreen;}else {leds[0] = CRGB::Black;}
+  if(RIGHT == true) {leds[1] = CRGB::Aquamarine;}else {leds[1] = CRGB::Black;}
+  if(UP == true) {leds[2] = CRGB::LightSkyBlue;}else {leds[2] = CRGB::Black;}
+  if(DOWN == true) {leds[3] = CRGB::DarkRed;}else {leds[3] = CRGB::Black;}
+  if(A == true) {leds[4] = CRGB::White;}else {leds[4] = CRGB::Black;}
+  if(B ==  true) {leds[5] = CRGB::Red;}else {leds[5] = CRGB::Black;}
   FastLED.show();
-  
+
 }
 
 void ReadButtons()
 {
-  LEFT = analogRead(A6);
-  RIGHT = analogRead(A5);
-  UP = analogRead(A4);
-  DOWN = analogRead(A7);
-  A = analogRead(A2);
-  B = analogRead(A3);
+  if(analogRead(A6) >500) {LEFT = true;} else {LEFT = false;}
+  if(analogRead(A5) >500) {RIGHT = true;} else {RIGHT = false;}
+  if(analogRead(A4) >500) {UP = true;} else {UP = false;}
+  if(analogRead(A7) >500) {DOWN = true;} else  {DOWN = false;}
+  if(analogRead(A2) >500) {A = true;} else  {A = false;}
+  if(analogRead(A3) >500) {B = true;} else  {B = false;}
+}
+
+void PrintButtons()
+{
+Serial.print("LEFT=");
+  Serial.print(LEFT);
+  Serial.print(" ");
+  Serial.print("RIGHT=");
+  Serial.print(RIGHT);
+  Serial.print(" ");
+  Serial.print("UP=");
+  Serial.print(UP);
+  Serial.print(" ");
+  Serial.print("DOWN=");
+  Serial.print(DOWN);
+  Serial.print(" ");
+  Serial.print("A=");
+  Serial.print(A);
+  Serial.print(" ");
+  Serial.print("B=");
+  Serial.print(B);
+  Serial.println(" ");
 }
 
 void PrintAnalogPorts()
